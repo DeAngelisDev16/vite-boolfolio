4ca27f00baa4e56e16049146c6232eb2b1a82c5d
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-06 08:43:36 +0100

    Router link in AppHeader commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,6 +17,30 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <div class="container-fluid">
+        <ul>
+            <li>
+                <router-link :to="{ name: 'home' }">
+                    Home</router-link>
+
+            </li>
+
+            <li>
+                <router-link :to="{ name: 'AboutUs' }">
+                    About Us</router-link>
+
+            </li>
+
+            <li>
+                <router-link :to="{ name: 'ProjectIndex' }">
+                    Index</router-link>
+
+            </li>
+
+
+        </ul>
+    </div>
+</template>
 
 <style scoped></style>
